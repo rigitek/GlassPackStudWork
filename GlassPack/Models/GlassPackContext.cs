242f5fcafaf31cfb746ec52b35d807e01f8de5cb
rigitek <rustemmukhametdinov000@gmail.com>
2024-09-20 23:06:24 +0500

    ijh

---
+++ GlassPack/Models/GlassPackContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlassPack.Models
+{
+    class GlassPackContext:DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public GlassPackContext()
+        {
+            //Database.EnsureDeleted();
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=GlassPack.db");
+        }
+    }
+}
---
+++ GlassPack/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlassPack.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public int Amount { get; set; }
+        public double Price { get; set; }
+
+        public int ArticleNum { get; set; }
+
+        public Brand? Brand { get; set; }
+        public Provider? Provider { get; set; }
+    }
+}
